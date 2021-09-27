eb5fc581675f245cbec55f559e3b271d7cd2d859
JuSe <js@JuSes-MBP.home>
2021-09-27 18:47:03 +0300

    code improvement: using useEffect to make arrays

--- components/Form.js
+++ components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Pressable, Alert } from 'react-native';
 import RadioForm from 'react-native-simple-radio-button';
 import DropDownPicker from 'react-native-dropdown-picker';
@@ -19,34 +19,23 @@ export default function Form() {
 
     const [open1, setOpen1] = useState(false);
     const [bottlePicker, setBottlePicker] = useState(null);
-    const [bottleAmount, setBottleAmount] = useState(
-        [
-            {label: "1 bottles", value: 1}, {label: "2 bottles", value: 2}, {label: "3 bottles", value: 3},
-            {label: "4 bottles", value: 4}, {label: "5 bottles", value: 5}, {label: "6 bottles", value: 6},
-            {label: "7 bottles", value: 7}, {label: "8 bottles", value: 8}, {label: "9 bottles", value: 9},
-            {label: "10 bottles", value: 10}, {label: "11 bottles", value: 11}, {label: "12 bottles", value: 12},
-            {label: "13 bottles", value: 13}, {label: "14 bottles", value: 14}, {label: "15 bottles", value: 15},
-            {label: "16 bottles", value: 16}, {label: "17 bottles", value: 17}, {label: "18 bottles", value: 18},
-            {label: "19 bottles", value: 19}, {label: "20 bottles", value: 20}, {label: "21 bottles", value: 21},
-            {label: "22 bottles", value: 22}, {label: "23 bottles", value: 23}, {label: "24 bottles", value: 24},
-            {label: "25 bottles", value: 25}
-        ]
-    );
+    const [bottleAmount, setBottleAmount] = useState([]);
 
     const [open2, setOpen2] = useState(false);
     const [timePicker, setTimePicker] = useState(null);
-    const [timeAmount, setTimeAmount] = useState(
-        [
-            {label: "1 hours", value: 1}, {label: "2 hours", value: 2}, {label: "3 hours", value: 3},
-            {label: "4 hours", value: 4}, {label: "5 hours", value: 5}, {label: "6 hours", value: 6},
-            {label: "7 hours", value: 7}, {label: "8 hours", value: 8}, {label: "9 hours", value: 9},
-            {label: "10 hours", value: 10}, {label: "11 hours", value: 11}, {label: "12 hours", value: 12},
-            {label: "13 hours", value: 13}, {label: "14 hours", value: 14}, {label: "15 hours", value: 15},
-            {label: "16 hours", value: 16}, {label: "17 hours", value: 17}, {label: "18 hours", value: 18},
-            {label: "19 hours", value: 19}, {label: "20 hours", value: 20}, {label: "21 hours", value: 21},
-            {label: "22 hours", value: 22}, {label: "23 hours", value: 23}, {label: "24 hours", value: 24}
-        ]
-    );
+    const [timeAmount, setTimeAmount] = useState([]);
+
+    useEffect(() => {
+        const bottleList = [];
+        const hoursList = [];
+        
+        for (let i = 0; i < 25; i++) {
+            bottleList.push({label: i + " bottles", value: i});
+            hoursList.push({label: i + " hours", value: i});
+        }
+        setBottleAmount(bottleList);
+        setTimeAmount(hoursList);
+    }, [])
 
     const showAlert = () => {
         Alert.alert(
